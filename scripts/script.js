22580c12a10249860b0ecb1ce3bb5b70eee88177
Vladimir Efremov <103284241+EfremovVA@users.noreply.github.com>
2024-04-10 14:05:53 +0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 document.getElementById("main-action").onclick = function () {
     document.getElementById("bush").scrollIntoView({behavior: "smooth"});
 }
+
+document.getElementById("item-action1").onclick = function () {
+    document.getElementById("zakaz").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("item-action2").onclick = function () {
+    document.getElementById("zakaz").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("item-action3").onclick = function () {
+    document.getElementById("zakaz").scrollIntoView({behavior: "smooth"});
+}
